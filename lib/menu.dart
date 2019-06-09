3f1a1808236a953e950473b420d720a2b9b4d5cf
Katie Tran <ktran182@my.yorku.ca>
2019-06-10 06:12:19 +0700

    - Bottom menu file

---
+++ lib/menu.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import './edit_screens/editDriverScreen.dart';
+import './show_info_screens/showDriverInfoScreen.dart';
+import './add_screens/addDriverScreen.dart';
+import './styles/styles.dart';
+
+class MyBottomMenu extends StatefulWidget {
+  MyBottomMenu ({Key key}) : super (key:key);
+  @override
+  _MyBottomMenuState createState() => _MyBottomMenuState();
+
+}
+
+class _MyBottomMenuState extends State<MyBottomMenu>{
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    ShowDriverInfo(
+      key: PageStorageKey('Page1'),
+    ),
+    Text(
+      'Hanh Trinh',
+      style: optionStyle,
+    ),
+    Text(
+      'Nhan Vien',
+      style: optionStyle,
+    ),
+    Text(
+      'Thong Bao',
+      style: optionStyle,
+    ),
+    Text(
+      'Ca Nhan',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type : BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            title: Text('Tài xế'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_library),
+            title: Text('Hành trình'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder_shared),
+            title: Text('Nhân viên'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            title: Text('Thông Báo'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            title: Text('Cá nhân'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xff0a2463),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
