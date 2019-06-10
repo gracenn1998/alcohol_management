3e49b4ca12596108e507394db4a3a5875fe368df
Grace <grace.nn1998@gmail.com>
2019-06-11 00:17:03 +0700

    add InkWell widget for onTap function

--- lib/show_info_screens/showAllDrivers.dart
+++ lib/show_info_screens/showAllDrivers.dart
@@ -38,64 +38,72 @@ Widget getListDriversView() {
     itemCount: listDrivers.length,
     itemBuilder: (context, index) {
       return
-      Container(
-        height: 120.0,
-          color: Colors.white,
-          child: Row(
-            children: <Widget>[
-              Container(
-                  padding: EdgeInsets.only(left: 15.0),
-                  child: CircleAvatar(
-                    radius: 45.0,
-                    backgroundImage: AssetImage('images/avatar.png'),
-                  )
-              ),
+        InkWell(
+          child: Container(
+              height: 120.0,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                      padding: EdgeInsets.only(left: 15.0),
+                      child: CircleAvatar(
+                        radius: 45.0,
+                        backgroundImage: AssetImage('images/avatar.png'),
+                      )
+                  ),
 
-              Expanded(
-                flex: 4,
-                child: Container(
+                  Expanded(
+                    flex: 4,
+                    child: Container(
 
-                    padding: EdgeInsets.only(left: 15.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.only(bottom: 10.0),
-                          child: Text(listDrivers[index], style: driverNameStyle()),
-                        ),
-                        Container(
-                          padding: EdgeInsets.only(bottom: 5.0),
-                          child: Row(
-                            children: <Widget>[
-                              Text("Trạng thái: ", style: driverStatusTitleStyle(0)),
-                              Text("Bình thường", style: driverStatusDataStyle(0)),
-                            ],
-                          ),
-                        ),
-                        Row(
+                        padding: EdgeInsets.only(left: 15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
-                            Text("Nồng độ cồn: ", style: driverStatusTitleStyle(0)),
-                            Text("0.5%", style: driverStatusDataStyle(0)),
+                            Container(
+                              padding: EdgeInsets.only(bottom: 10.0),
+                              child: Text(listDrivers[index], style: driverNameStyle()),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(bottom: 5.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Text("Trạng thái: ", style: driverStatusTitleStyle(0)),
+                                  Text("Bình thường", style: driverStatusDataStyle(0)),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text("Nồng độ cồn: ", style: driverStatusTitleStyle(0)),
+                                Text("0.5%", style: driverStatusDataStyle(0)),
+                              ],
+                            ),
                           ],
-                        ),
-                      ],
-                    )
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Icon(Icons.delete, size: 40.0, color: Color(0xff0A2463),),
+                        )
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Icon(Icons.delete, size: 40.0, color: Color(0xff0A2463),),
+                  )
+
+
+
+
+
+                ],
               )
 
+          ),
+          onTap: () {
+            //go to detail info
+            debugPrint("driver tapped");
+          },
+        )
+        ;
 
-
-
-
-            ],
-          )
-
-      );
     },
     separatorBuilder: (context, index) {
       return Divider(height: 1.0,);
