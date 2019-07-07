af665637d7481029a6e7846e0907434bff7b6d3f
Bui Xuan Huynh <buixuanhuynha1@gmail.com>
2019-07-07 22:17:45 +0700

    move DriverInfo and JourneyInfo func from Working to TD-stylesnfunc

--- lib/show-trip-details/WorkingTripDetail.dart
+++ lib/show-trip-details/WorkingTripDetail.dart
@@ -19,18 +19,50 @@ class WorkingTripDetailState extends State<WorkingTripDetail> with SingleTickerP
   var _trip;
   WorkingTripDetailState(this.jID);
 
-  GlobalKey _keyRed = GlobalKey();
   PermissionStatus _status;
   AnimationController _animationController;
   int _selectedIndex = 0;
+
   static double JourneyInfoHeight = 190.0;
   // Can tim cach tinh chieu cao cua JourneyInfo() widget =.="
 
+//////////GET USER LOCATION
   Completer<GoogleMapController> _controller = Completer();
   Map<String, double> curLocation;
   var location = new Location();
 
-  @override
+  Future<Map<String, double>> _getLocation() async{
+    curLocation  = <String, double>{};
+    try {
+      curLocation = await location.getLocation();
+      setState(() {
+
+      });
+    } catch(e) {
+      curLocation = null;
+    }
+    return curLocation;
+  }
+
+  Widget buildMap(){
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition:
+      CameraPosition(
+        target: curLocation == null ?
+        LatLng(10.03711, 105.78825): //Can Tho City
+        LatLng(curLocation["latitude"], curLocation["longitude"]), //user location
+        zoom: 15,
+      ),
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+      myLocationEnabled : true,
+    );
+  }
+
+
+//////////------------------------------------------------------
 
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -81,30 +113,12 @@ class WorkingTripDetailState extends State<WorkingTripDetail> with SingleTickerP
     );
   }
 
-  Widget buildMap(){
-
-    return GoogleMap(
-      mapType: MapType.normal,
-      initialCameraPosition:
-      CameraPosition(
-        target: curLocation == null ?
-        LatLng(10.03711, 105.78825): //Can Tho City
-        LatLng(curLocation["latitude"], curLocation["longitude"]), //user location
-        zoom: 15,
-      ),
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
-      myLocationEnabled : true,
-    );
-  }
-
   Widget _buildStack(BuildContext context, BoxConstraints constraints) {
     final Animation<RelativeRect> animation = _getPanelAnimation(constraints);
 
     return new Column(
       children: <Widget>[
-        DriverInfo(),
+        DriverInfo(_trip),
         new Stack(
           children: <Widget>[
             new Container(
@@ -113,7 +127,7 @@ class WorkingTripDetailState extends State<WorkingTripDetail> with SingleTickerP
                   height: constraints.biggest.height - 120
               ),
             ),
-            JourneyInfo(),
+            JourneyInfo(_trip),
             new PositionedTransition(
               rect: animation,
               child: new Material(
@@ -135,300 +149,17 @@ class WorkingTripDetailState extends State<WorkingTripDetail> with SingleTickerP
     );
   }
 
-  Widget DriverInfo(){
-    return Container(
-        height: 120.0,
-        color: Colors.white,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.only(left: 15.0),
-                child: CircleAvatar(
-                  radius: 45.0,
-                  backgroundImage: AssetImage('images/avatar.png'),
-                )), // Avatar
+  void initState(){
+    _getLocation();
 
-            Container(
-                padding: EdgeInsets.only(left: 15.0, right: 15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.only(bottom: 10.0),
-                      child: Text(_trip['name'],
-                          style: driverNameStyleinJD()),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(bottom: 10.0),
-                      child: Text("Chỉ số cồn ",
-                          style: timeStyleinJD()),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(bottom: 5.0),
-                      child: Row(
-                        children: <Widget>[
-                          Text("Ban đầu: ",
-                              style: driverStatusTitleStyle(0)),
-                          Text("100",
-                              style: driverStatusDataStyle(0)),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      children: <Widget>[
-                        Text("Hiện tại: ",
-                            style: driverStatusTitleStyle(0)),
-                        Text("500", style: driverStatusDataStyle(1)),
-                      ],
-                    ),
-                  ],
-                )),
-            //Ten + Trang thai
-            Expanded(
+    super.initState();
+    _animationController = new AnimationController(
+        duration: const Duration(milliseconds: 100), value: 1.0, vsync: this);
 
-              //padding: EdgeInsets.only(left: 5.0),
-              child: Container(
-                child: RaisedButton(
-                  child: Text("XỬ LÝ", style: TextStyle(color: Colors.white),),
-                  color: Color(0xffef3964),
-                  onPressed: () {
+    PermissionHandler().checkPermissionStatus(PermissionGroup.locationWhenInUse)
+        .then(_updateStatus);
 
-                    print("XULYYYYYYYYyyy");
-                    heightOfJourneyInfo();
-                    print(JourneyInfoHeight);
-                    //
-//                      final RenderBox renderBoxRed = _keyRed.currentContext.findRenderObject();
-//                      final sizeRed = renderBoxRed.size;
-//                      print("SIZE of Red: ${sizeRed.height} ");
-                  },
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 30.0),
-            )
-          ],
-        )
-    );
   }
-
-  Widget JourneyInfo(){
-    return Container(
-      color: Colors.white,
-      child: Column(
-        key: _keyRed,
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.only(left: 15.0, top: 5.0),
-                  child: Text( _trip['jID'],
-                    //document[index].documentID,
-                    style: const TextStyle(
-                        color: const Color(0xff000000),
-                        fontWeight: FontWeight.w900,
-                        fontFamily: "Roboto",
-                        fontStyle: FontStyle.normal,
-                        fontSize: 28.0
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          //1
-
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.only(left: 15.0, top: 5.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.event,
-                        color: Color(0xff8391b3),
-                        size: 23.0,
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(left: 5.0),
-                        child: Text( //"20",
-                            formatDateTime(_trip['schStart']), //document[index].data['schStart']
-                            style: timeStyleinJD()
-//                              TextStyle(
-//                                  color: Color(0xff0a2463),
-//                                  fontWeight: FontWeight.w400,
-//                                  fontFamily: "Roboto",
-//                                  fontStyle: FontStyle.normal,
-//                                  fontSize: 15.0)
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ), //2
-
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                    padding: EdgeInsets.only(left: 15.0, top: 10.0),
-                    child: Text(
-                        "Từ:",
-                        style: const TextStyle(
-                            color:  const Color(0xff8391b3),
-                            fontWeight: FontWeight.w400,
-                            fontFamily: "Roboto",
-                            fontStyle:  FontStyle.normal,
-                            fontSize: 14.0
-                        )
-                    )
-                ),
-              ),
-
-              Expanded(
-                flex: 1,
-                child: Container(
-                    padding: EdgeInsets.only(left: 15.0, top: 10.0),
-                    child: Text(
-                        "Đến:",
-                        style: const TextStyle(
-                            color:  const Color(0xff8391b3),
-                            fontWeight: FontWeight.w400,
-                            fontFamily: "Roboto",
-                            fontStyle:  FontStyle.normal,
-                            fontSize: 14.0
-                        )
-                    )
-                ),
-              ),
-            ],
-          ), //3
-
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.only(left: 15.0, top: 1.0),
-                  child:
-                  Text(
-                    _trip['from'],
-                    // document[index].data['from'],
-                    style: TextStyle(
-                        color:  Color(0xff000000),
-                        fontWeight: FontWeight.w700,
-                        fontFamily: "Roboto",
-                        fontStyle:  FontStyle.normal,
-                        fontSize: 14.0
-                    ),
-                  ),
-                ),
-              ),
-              Icon(Icons.arrow_forward_ios),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.only(left: 5.0, right: 15.0, top: 1.0),
-                  child:
-                  Text(
-                    _trip['to'],
-
-                    style: TextStyle(
-                        color:  Color(0xff000000),
-                        fontWeight: FontWeight.w700,
-                        fontFamily: "Roboto",
-                        fontStyle:  FontStyle.normal,
-                        fontSize: 14.0
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ), //4
-
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                    padding: EdgeInsets.only(left: 15.0, top: 10.0),
-                    child: Text(
-                        "Bắt đầu lúc:",
-                        style: const TextStyle(
-                            color:  const Color(0xff8391b3),
-                            fontWeight: FontWeight.w400,
-                            fontFamily: "Roboto",
-                            fontStyle:  FontStyle.normal,
-                            fontSize: 14.0
-                        )
-                    )
-                ),
-              ),
-
-              Expanded(
-                flex: 1,
-                child: Container(
-                    padding: EdgeInsets.only(left: 15.0, top: 10.0),
-                    child: Text(
-                        "Đã chạy được:",
-                        style: const TextStyle(
-                            color:  const Color(0xff8391b3),
-                            fontWeight: FontWeight.w400,
-                            fontFamily: "Roboto",
-                            fontStyle:  FontStyle.normal,
-                            fontSize: 14.0
-                        )
-                    )
-                ),
-              ),
-            ],
-          ), //5
-
-          Row(
-            children: <Widget>[
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.only(left: 15.0, top: 1.0),
-                  child:
-                  Text(
-                      formatDateTime(_trip['start']),
-                      style: timeStyleinJD()
-                  ),
-                ),
-              ),
-              Icon(Icons.arrow_forward_ios),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  padding: EdgeInsets.only(left: 5.0, right: 15.0, top: 1.0),
-                  child:
-                  Text(
-                      fromStartTime(_trip['start']),
-                      style: timeStyleinJD()
-                  ),
-                ),
-              )
-            ],
-          ), //6
-
-        ],
-      ),
-    );
-  }
-
 
   //ANIMATIONNNNNNNNNNNNNNNNN
   bool get _isPanelVisible {
@@ -437,7 +168,6 @@ class WorkingTripDetailState extends State<WorkingTripDetail> with SingleTickerP
         status == AnimationStatus.forward;
   }
 
-  @override
   void dispose() {
     super.dispose();
     _animationController.dispose();
@@ -456,28 +186,7 @@ class WorkingTripDetailState extends State<WorkingTripDetail> with SingleTickerP
     ).animate(new CurvedAnimation(parent: _animationController, curve: Curves.linear));
   }
 
-  void heightOfJourneyInfo(){
-    final RenderBox renderBoxRed = _keyRed.currentContext.findRenderObject();
-    final sizeRed = renderBoxRed.size;
-    print("SIZE of Red: ${sizeRed.height} ");
-    JourneyInfoHeight = sizeRed.height;
-  }
-
-
-  void initState(){
-    _getLocation();
-
-    super.initState();
-    _animationController = new AnimationController(
-        duration: const Duration(milliseconds: 100), value: 1.0, vsync: this);
-
-    PermissionHandler().checkPermissionStatus(PermissionGroup.locationWhenInUse)
-        .then(_updateStatus);
-
-  }
-
   //LOCATION ACCESS PERMISSIONNNNNNNNNN
-
   void _updateStatus(PermissionStatus status){
     //print("$status");
     if (status != _status)
@@ -487,10 +196,10 @@ class WorkingTripDetailState extends State<WorkingTripDetail> with SingleTickerP
       });
     }
   }
+
   void _askPermission(){
     PermissionHandler().requestPermissions([PermissionGroup.locationWhenInUse])
         .then(_onStatusRequested);
-
   }
 
   void _onStatusRequested(Map <PermissionGroup, PermissionStatus> statuses ){
@@ -500,20 +209,6 @@ class WorkingTripDetailState extends State<WorkingTripDetail> with SingleTickerP
       PermissionHandler().openAppSettings();
 
     _updateStatus(status);
-  }
-
-//////////GET USER LOCATION
-  Future<Map<String, double>> _getLocation() async{
-    curLocation  = <String, double>{};
-    try {
-      curLocation = await location.getLocation();
-      setState(() {
-
-      });
-    } catch(e) {
-      curLocation = null;
-    }
-    return curLocation;
   }
 
 }
