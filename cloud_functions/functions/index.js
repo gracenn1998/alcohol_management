427968d809552db1c03ecf20e41ab3fe71ded783
Grace <grace.nn1998@gmail.com>
2019-09-07 22:29:22 +0700

    modified noti logics

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -6,58 +6,15 @@ admin.initializeApp();
 const drunkVal = 400;
 const delayTime = 1000*60;
 
-//exports.senNoti= functions.database.ref('driver/{dID}/alcoholVal').onUpdate((change, context) => {
-//    const dID = context.params.dID;
-//    const alcoVal = change.after.val();
-//    const curTime = new Date();
-//
-//    admin.database().ref("/driver/" + dID).once('value').then((snapshot) => {
-//        var lastNotiTime = snapshot.child('lastNotiTime').val();
-//        var tripID = snapshot.child('tripCode').val();
-//        console.log(curTime.getTime() - lastNotiTime);
-//
-//        if(alcoVal >= drunkVal && (curTime.getTime() - lastNotiTime)>= delayTime) {
-//                var msg = {
-//                        notification: {
-//                            title: 'Vượt mức chỉ số cồn',
-//                            body: 'Tài xế ' + dID + ' có dấu hiệu vượt mức nồng độ cồn',
-//                        },
-//                        data : {
-//                            'lastNotiTime' : lastNotiTime.toString(),
-//                            'dID' : dID.toString(),
-//                            'tripID' : tripID.toString()
-//                        }
-//                    }
-//                admin.messaging().sendToTopic('alcoholTracking', msg).then((response) => {
-//                    console.log("Success", response);
-//                    lastNotiTime = curTime.getTime();
-//                    admin.database().ref("/driver/" + dID).update({lastNotiTime});
-//                })
-//                .catch((error) => {
-//                    console.log("Error", error);
-//                    return false;
-//                });
-//            }
-//            else{
-//                console.log('still in delay')
-//            }
-////        console.log(snapshot.child('tripCode').val());
-//    });
-//
-//    return true;
-//
-//});
 
-const db = admin.firestore();
-
-exports.sendNoti = functions.database.ref('driver/{dID}/alcoholVal').onUpdate((change, context) => {
+exports.processNoti = functions.database.ref('driver/{dID}/notiInfo/alcoholVal').onUpdate((change, context) => {
     const dID = context.params.dID;
     const alcoVal = change.after.val();
     const curTime = new Date();
 
     admin.database().ref("/driver/" + dID).once('value').then((snapshot) => {
-            var lastNotiTime = snapshot.child('lastNotiTime').val();
-            var tripID = snapshot.child('tripID').val();
+            var lastNotiTime = snapshot.child('notiInfo/lastNotiTime').val();
+            var curTripID = snapshot.child('tripID').val();
             console.log(curTime.getTime() - lastNotiTime);
 
             if(alcoVal >= drunkVal && (curTime.getTime() - lastNotiTime)>= delayTime) {
@@ -69,57 +26,34 @@ exports.sendNoti = functions.database.ref('driver/{dID}/alcoholVal').onUpdate((c
                             data : {
                                 'lastNotiTime' : lastNotiTime.toString(),
                                 'dID' : dID.toString(),
-                                'tripID' : tripID.toString()
+                                'tripID' : curTripID.toString()
                             }
                         }
+                    //send noti
                     admin.messaging().sendToTopic('alcoholTracking', msg).then((response) => {
                         console.log("Success", response);
                         lastNotiTime = curTime.getTime();
                         admin.database().ref("/driver/" + dID).update({lastNotiTime});
-
                     })
                     .catch((error) => {
                         console.log("Error", error);
                         return false;
                     });
+
+                    //log noti
+                    var notiID = dID+lastNotiTime;
+                    admin.database().ref("/bnotification/" + notiID).set({
+                        body: "Tài xế " + dID + " có dấu hiệu vượt mức nồng độ cồn",
+                        dID : dID,
+                        isTapped: false,
+                        timeCreated: lastNotiTime,
+                        tripID: curTripID
+                    })
                 }
                 else{
                     console.log('still in delay')
                 }
-    //        console.log(snapshot.child('tripCode').val());
         });
 
     return true;
 });
-
-
-exports.logNoti = functions.database.ref('driver/{dID}').onUpdate((change, context) => {
-//    console.log('?????');
-    const dID = context.params.dID;
-//    const lastNotiTime = change.after.val();
-    const newNotiTime = change.after.val()['lastNotiTime'];
-    const oldNotiTime = change.before.val()['lastNotiTime'];
-
-    if(newNotiTime != oldNotiTime) {
-        const tripID = change.after.val()['tripID'];
-
-        var doc = db.collection('bnotification').doc(dID+newNotiTime);
-            return doc
-                  .set({
-                    body: "Tài xế " + dID + " có dấu hiệu vượt mức nồng độ cồn",
-                    dID : dID,
-                    isTapped: false,
-                    timeCreated: newNotiTime.toString(),
-                    tripID: tripID
-                  })
-                  .then(() => {
-                    return { result: 'document updated' };
-                  })
-                  .catch(function(error) {
-                    console.error("Error writing document: ", error);
-                   });
-    }
-
-
-    }
-);
