1270ffac4cd89ccfebaf04855c94c73f9bf187e4
Bui Xuan Huynh <buixuanhuynha1@gmail.com>
2019-09-07 07:47:03 +0700

    Create add trip screen

---
+++ lib/add_screens/addTripScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AddTrip extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AddTripState();
+  }
+}
+
+class _AddTripState extends State<AddTrip> {
+
+  Widget build(BuildContext build)
+  {
+
+  }
+}
